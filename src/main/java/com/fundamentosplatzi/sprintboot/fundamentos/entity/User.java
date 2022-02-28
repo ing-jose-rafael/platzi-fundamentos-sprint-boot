e65eae1874de2fb547c98db94d4ff8eaa0ae0ad0
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-27 21:25:36 -0500

    modelando con JPA, relacion Many to One y One to Many

---
+++ src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/entity/User.java
@@ -0,0 +1,88 @@
+package com.fundamentosplatzi.sprintboot.fundamentos.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "id_user",unique = true,nullable = false)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+    @Column(length = 50)
+    private String email;
+
+    private LocalDate birthDate;
+    /* representando la relacion un usuario muchos post */
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Post> posts = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", posts=" + posts +
+                '}';
+    }
+}
