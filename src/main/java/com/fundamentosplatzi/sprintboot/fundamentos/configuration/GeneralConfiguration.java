d8d309ec712222833455120e73d5bc87b1f551a8
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-27 21:50:03 -0500

    configuracion dataSource a nivel de clase, manual

--- src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentosplatzi.sprintboot.fundamentos.bean.MyBeanWithPropertiesImp
 import com.fundamentosplatzi.sprintboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -24,5 +27,17 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name,apellido);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+
+        return dataSourceBuilder.build();
+
     }
 }
