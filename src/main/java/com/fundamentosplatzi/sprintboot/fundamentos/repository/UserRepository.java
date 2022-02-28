711e18a7489c6189b373a78ed01bcc8ce53235db
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-02-27 22:49:26 -0500

    creando interfaz de UserRepository y PostRepository

--- src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/repository/UserRepository.java
+++ src/main/java/com/fundamentosplatzi/sprintboot/fundamentos/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.fundamentosplatzi.sprintboot.fundamentos.repository;
 
 import com.fundamentosplatzi.sprintboot.fundamentos.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
